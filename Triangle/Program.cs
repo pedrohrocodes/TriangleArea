4b5f52dbd3340218561c8cf96d535be4f5a12a23
Pedro <pedro.oliveira@dbserver.com.br>
2023-03-03 13:02:09 -0300

    :tada: First Commit

---
+++ Triangle/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Globalization;
+
+namespace Triangle {
+    class Program {
+        static void Main(string[] args) {
+            var triangleX = new Triangle();
+            var triangleY = new Triangle();
+
+            Console.WriteLine("Entre com as medidas do triângulo X:");
+            triangleX.SetSideA(double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture));
+            triangleX.SetSideB(double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture));
+            triangleX.SetSideC(double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture));
+
+            Console.WriteLine("Entre com as medidas do triângulo Y:");
+            triangleY.SetSideA(double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture));
+            triangleY.SetSideB(double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture));
+            triangleY.SetSideC(double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture));
+
+
+            double areaX = triangleX.CalculateArea();
+            double areaY = triangleY.CalculateArea();
+
+            Console.WriteLine("Área de X = " + areaX.ToString("F4", CultureInfo.InvariantCulture));
+            Console.WriteLine("Área de Y = " + areaY.ToString("F4", CultureInfo.InvariantCulture));
+
+            if (areaX > areaY) {
+                Console.WriteLine("Maior área: X");
+            } else {
+                Console.WriteLine("Maior área: Y");
+            }
+        }
+    }
+}
---
+++ Triangle/Triangle.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace Triangle {
+    // Classe não anêmica
+    public class Triangle {
+        public double A { get; private set; }
+        public double B { get; private set; }
+        public double C { get; private set; }
+
+        public Triangle() {
+
+        }
+
+        public Triangle(double a, double b, double c) {
+            this.A = a;
+            this.B = b;
+            this.C = c;
+        }
+
+        public void SetSideA(double a) {
+            this.A = a;
+        }
+
+        public void SetSideB(double b) {
+            this.B = b;
+        }
+
+        public void SetSideC(double c) {
+            this.C = c;
+        }
+
+        public double CalculateArea() {
+            double p = (A + B + C) / 2.0;
+            return Math.Sqrt(p * (p - A) * (p - B) * (p - C));
+        }
+    }
+}
